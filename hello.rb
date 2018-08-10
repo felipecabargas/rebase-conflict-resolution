e3b7f9d21f2d5b97dc5f04e9d1d66c68710c5ee3
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2018-08-10 14:45:05 -0400

    ADD: spanish support

--- hello.rb
+++ hello.rb
@@ -1,9 +1,15 @@
 class Hello
 
-  def world
+  def world language='en'
     a = "Hello"
     b = " World"
-    puts a.concat(b)
+    a_es = "Hola"
+    b_es = "Mundo"
+    if language == 'en'
+      puts a.concat(b)
+    else
+      puts a_es + " " + b_es
+    end
   end
 
 end
