42869e8509f8559b3af5c015cb23b6a93bcbbcc1
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2018-08-10 16:43:23 -0400

    FIX: concat

--- hello.rb
+++ hello.rb
@@ -4,11 +4,11 @@ class Hello
     a = "Hello"
     b = " World"
     a_es = "Hola"
-    b_es = "Mundo"
+    b_es = " Mundo"
     if language == 'en'
       puts a.concat(b)
     else
-      puts a_es + " " + b_es
+      puts a_es.concat(b_es)
     end
   end
 
